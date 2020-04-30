173dc0a00a6ec4be3ba646c5c227bdd6322660ac
Valdes-Tresanco-MS <mariosergiovaldes145@gmail.com>
2020-04-30 16:10:56 -0500

    improved installer

--- install.sh
+++ install.sh
@@ -54,9 +54,12 @@ install_directory=`zenity --file-selection --title="Select directory for AMDock 
 
 ## check that AMDock files not exits in destiny
 pymol_plugin=$HOME/.pymol/startup
-if [ ! -d "~/.pymol" ]
+if [ ! -d "~/.pymol/$pymol_plugin" ]
     then
-    mkdir $HOME/.pymol
+      if [ ! -d "~/.pymol/" ]
+        then
+          mkdir $HOME/.pymol
+      fi
     mkdir $pymol_plugin
 fi
 # change permissions of directory
